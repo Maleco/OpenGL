1ef15615c562b0653d4e10bb6ac10f914bf99f9e
Maikel Withagen <maikel.withagen@gmail.com>
2012-03-23 12:22:22 +0100

    final week 4

--- openglframework/fragmentshader.glsl
+++ openglframework/fragmentshader.glsl
@@ -2,21 +2,28 @@
 varying vec3 N;
 varying vec3 V;
 
+#define ALPHA 0.5
+#define BETA 0.5
+#define BLUE 0.6
+#define YELLOW 0.3
+
 void main () {
-	//The normal vector
-	vec3 normalN = normalize(N);
-	//The reflected vector of V and N
-	vec3 R = reflect(normalize(V), normalN);
-	//The light vector
-	vec3 L = normalize(vec3(gl_LightSource[0].position));
+		//The normal vector
+		vec3 normalN = normalize(N);
+		//The reflected vector of V and N
+		vec3 R = reflect(normalize(V), normalN);
+		//The light vector
+		vec3 L = normalize(vec3(gl_LightSource[0].position));
 
-	//The ambiant light calculation
-	vec4 ambient = gl_LightSource[0].ambient * gl_FrontMaterial.ambient;
-	//The diffuse light calculation
-	vec4 diffuse = max(dot(L, N), 0.0) * gl_LightSource[0].diffuse * gl_FrontMaterial.diffuse;
-	//The specular light calculation
-	vec4 specular= pow(max(dot(R,L), 0.0), 0.7*gl_FrontMaterial.shininess) * gl_LightSource[0].specular * gl_FrontMaterial.specular;
+		vec4 kCool = vec4(0.0,0.0,BLUE,1.0) + ALPHA*gl_LightSource[0].diffuse * gl_FrontMaterial.diffuse ;
+		vec4 kWarm = vec4(YELLOW,YELLOW,0.0,1.0) + BETA*gl_LightSource[0].diffuse * gl_FrontMaterial.diffuse;
 
-	//Determening the color using al the calculations
-	gl_FragColor = vec4(ambient + diffuse + specular);
+		//The diffuse light calculation
+		vec4 diffuse = kCool *(1 - dot(N,L))/2 + kWarm * (1 + dot(N,L))/2;
+
+		//The specular light calculation
+		vec4 specular= pow(max(dot(R,L), 0.0), 0.7*gl_FrontMaterial.shininess) * gl_LightSource[0].specular * gl_FrontMaterial.specular;
+
+		//Determening the color using al the calculations
+	gl_FragColor = vec4(diffuse + specular);
 }
--- openglframework/openglframework.c
+++ openglframework/openglframework.c
@@ -29,7 +29,6 @@
 #include <GL/glut.h>
 #endif
 
-#include "glm.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -41,7 +40,7 @@ typedef int bool;
 #define false 0
 #define SPHERE_N (20)
 #define apertureSamples 1.0
-#define CONST_C 10 / sqrt(apertureSamples)
+#define CONST_C 0 / sqrt(apertureSamples)
 #define GOLDEN_ANGLE 137.508
 #define eyeDistance	1000
 #define focusDistance 200
@@ -49,9 +48,6 @@ typedef int bool;
 int oldX, oldY, deltaX, deltaY;		//Keep track of mouse-movements
 bool Rotate, Zoom;			//Keep track of transformations
 float zoomLevel;
-GLMmodel *model;
-GLuint vboDots;
-GLuint vboTriangles;
 
 ///Functions
 void setGlMaterial(GLfloat r, GLfloat g, GLfloat b, GLfloat ka, GLfloat kd, GLfloat ks, GLfloat n)
@@ -65,89 +61,95 @@ void setGlMaterial(GLfloat r, GLfloat g, GLfloat b, GLfloat ka, GLfloat kd, GLfl
     glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, n);
 }
 
-void glmInitVBO(GLMmodel *model)
+
+void init(void)
 {
-	glGenBuffersARB(1, &vboDots);
-	glGenBuffersARB(1, &vboTriangles);
+   GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+   GLfloat mat_shininess[] = { 50.0 };
 
-	//Hold the model's vertices (points)
-	//vboDots = calloc(model->numvertices, sizeof(GLuint));
-	//vboDots = model->vertices;
+   GLfloat light_position[] = {-200.0,600.0,1500.0, 1.0};
+   GLfloat light_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
+   GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+   GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
 
-	//Hold the model's triangles
-	//vboTriangles = calloc(model->numtriangles, sizeof(GLuint));
-	//vboTriangles = model->triangles;
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+   glShadeModel (GL_SMOOTH);
 
-	glBindBufferARB(GL_ARRAY_BUFFER_ARB, vboDots);
-	glBufferDataARB(GL_ElEMENT_ARRAY_BUFFER_ARB, sizeof(model->numvertices), model->numvertices, GL_STATIC_DRAW_ARB);
+   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+   glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+   glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+   glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 
-	glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, vboTriangles);
-	glBufferDataARB(GL_ELEMENT_ARRAY_BUFFER_ARB, sizeof(model->triangles), model->triangles, GL_STATIC_DRAW_ARB);
-
-
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
+   glEnable(GL_DEPTH_TEST);
+   initGLSLProgram("vertexshader.glsl","fragmentshader.glsl");
 }
 
-void glmDrawVBO(){
-
-}
-
-void init(void){
-
-	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat mat_shininess[] = { 50.0 };
-
-	GLfloat light_position[] = {-200.0,600.0,1500.0, 1.0};
-	GLfloat light_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-
-	glClearColor (0.0, 0.0, 0.0, 0.0);
-	glShadeModel (GL_SMOOTH);
-
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-
-
-	model = glmReadOBJ("obj/devilduk.obj");
-	if(!model) printf("Object niet geladen\n");
-	glmFacetNormals(model);
-	glmVertexNormals(model, 90.0 );
-
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_DEPTH_TEST);
-	initGLSLProgram("vertexshader.glsl","fragmentshader.glsl");
-}
 
 
 void display(void){
+	float i;
+	float x = 0.0, y = 0.0;
 
-	/* Clear all pixels */
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	gluLookAt(0.0,0.0,3.0,0.0,0.0,0.0,0.0,1.0,0.0);
+	glClear(GL_ACCUM_BUFFER_BIT);
 
-	//Zoom
-	glTranslatef(1.0, 1.0, zoomLevel);
+	for(i = 0.0; i < apertureSamples; i++){
+		float r = CONST_C * sqrt(i);
+		float theta = i * GOLDEN_ANGLE;
+		x = r * cos(theta);
+		y = r * sin(theta);
 
-	//Rotate around origin
-	glRotatef(deltaY,1,0,0);
-	glRotatef(deltaX,0,1,0);
+		/* Clear all pixels */
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+		glLoadIdentity();
+		gluLookAt(200.0+x,200.0+y,eyeDistance,200.0,200.0,focusDistance,0.0,1.0,0.0);
 
-	//Draw model
-	glmUnitize(model);
-	glmScale(model, 2);
-	glmDraw(model, GLM_SMOOTH);
-	glPushMatrix();
+		//Zoom
+		glTranslatef(1.0, 1.0, zoomLevel*100);
 
-	glAccum(GL_ACCUM, (1/apertureSamples));
-	glFlush();
+		//Rotate around point (200,200,200)
+		glTranslatef(200, 200, 200);
+		glRotatef(deltaY,1,0,0);
+		glRotatef(deltaX,0,1,0);
+		glTranslatef(-200, -200, -200);
 
-	glutSwapBuffers();
+		//Insert spheres
+		setGlMaterial(0.0f,0.0f,1.0f,0.2,0.7,0.5,64);
+		glPushMatrix();
+		glTranslated(90,320,100);
+		glutSolidSphere(50,SPHERE_N,SPHERE_N);
+		glPopMatrix();
+
+		setGlMaterial(0.0f,1.0f,0.0f,0.2,0.3,0.5,8);
+		glPushMatrix();
+		glTranslated(210,270,300);
+		glutSolidSphere(50,SPHERE_N,SPHERE_N);
+		glPopMatrix();
+
+		setGlMaterial(1.0f,0.0f,0.0f,0.2,0.7,0.8,32);
+		glPushMatrix();
+		glTranslated(290,170,150);
+		glutSolidSphere(50,SPHERE_N,SPHERE_N);
+		glPopMatrix();
+
+		setGlMaterial(1.0f,0.8f,0.0f,0.2,0.8,0.0,1);
+		glPushMatrix();
+		glTranslated(140,220,400);
+		glutSolidSphere(50,SPHERE_N,SPHERE_N);
+		glPopMatrix();
+
+		setGlMaterial(1.0f,0.5f,0.0f,0.2,0.8,0.5,32);
+		glPushMatrix();
+		glTranslated(110,130,200);
+		glutSolidSphere(50,SPHERE_N,SPHERE_N);
+		glPopMatrix();
+
+		glAccum(GL_ACCUM, (1/apertureSamples));
+		glFlush();
+	}
+	glAccum(GL_RETURN, 1.0);
+    glutSwapBuffers();
 }
 
 void mouse(int button, int state, int x, int y){
@@ -198,7 +200,7 @@ void reshape(int w, int h){
     glViewport(0,0, (GLsizei) w, (GLsizei) h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60.0,(GLdouble)w/(GLdouble)h,1.5,20.0);
+    gluPerspective(2.0*atan2(h/2.0,1000.0)*180.0/M_PI,(GLdouble)w/(GLdouble)h,500,1000);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -210,8 +212,8 @@ int main(int argc, char** argv)
 
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_ACCUM);
-    glutInitWindowSize(800,800);
-    glutInitWindowPosition(400, 300);
+    glutInitWindowSize(400,400);
+    glutInitWindowPosition(220,100);
     glutCreateWindow("Computer Graphics - OpenGL framework");
 
 #if defined(NEED_GLEW)
